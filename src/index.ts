e87be5a307d9e8d2c6d5224dde2cf58ffd7f2281
Pascal Seitz <pascal.seitz@sap.com>
2020-10-30 12:37:24 +0100

    add debug, fix to_path

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as yargs from 'yargs'
 import glob from 'glob'
 import fs from 'fs'
 import path from 'path'
-
+const { sep } = require('path')
 const argv = yargs
     .options({
         from: {
@@ -12,21 +12,31 @@ const argv = yargs
         },
         to: { type: 'string', describe: 'the target path, e.g. test_project/dist/' },
         filter: { type: 'string', describe: 'a regex to filter files' },
+        debug: { type: 'boolean', describe: 'print debug information' },
     })
     .demandOption(['from', 'to'], 'Please provide start argument to work with this tool').argv
 
 let files = glob.sync(argv.from)
-// console.log(files)
+if( argv.debug){
+    console.log("Glob Files")
+    console.log(files)
+}
 if (argv.filter) {
     let regex = new RegExp(argv.filter)
     files = files.filter(regex.test)
+
+    if( argv.debug){
+        console.log("Filtered Files")
+        console.log(files)
+    }
 }
 
 let glob_star_root = argv.from.split("**");
 
 if(glob_star_root.length > 1){
+    let to_folder = argv.to.endsWith(sep) ? argv.to : argv.to + sep;
     let copy = files.map(file => 
-        ({from: file, to: file.replace(glob_star_root[0], argv.to)})
+        ({from: file, to: file.replace(glob_star_root[0], to_folder)})
     )
 
     for (const blub of copy) {
